51d81ea9a410de6974c4a3c21186b25cfe7fb585
Mikael Karon <mikael@karon.se>
2018-09-28 17:26:00 +0200

    stay DRY

--- controller.js
+++ controller.js
@@ -1,8 +1,27 @@
 const mime = require('mime-types');
 const { NEXT, ROUTE, TYPE, CACHE_SHORT, CACHE_LONG } = require('./const');
 
+const process = async (req, res, result, cache_control, etag, options) => {
+  const { cache, mime } = options;
+
+  if (result) {
+    if (cache) {
+      res.set({
+        'Cache-Control': cache_control,
+        'ETag': etag
+      });
+    }
+    if (mime) {
+      res.type(mime.lookup(req.path) || TYPE);
+    }
+    res.send(result);
+  }
+
+  return !!result;
+}
+
 module.exports = (repo, options) => ({
-  refToTree: async(req) => {
+  refToTree: async (req) => {
     const { tree } = req.params;
 
     if (req.ref = await repo.getRef(`refs/heads/${tree}`)) {
@@ -14,41 +33,15 @@ module.exports = (repo, options) => ({
 
   loadPath: async (req, res) => {
     const { path, tree } = req.params;
+    const { cache_long = CACHE_LONG, cache_short = CACHE_SHORT } = options;
 
-    const result = await repo.loadTextByPath(tree, path);
-    if (result) {
-      if (options.cache) {
-        res.set({
-          'Cache-Control':  req.ref ? options.cache_short || CACHE_SHORT : options.cache_long || CACHE_LONG,
-          'ETag': tree
-        });
-      }
-      if (options.mime) {
-        res.type(mime.lookup(req.path) || TYPE);
-      }
-      res.send(result);
-    }
-
-    return result ? NEXT : ROUTE;
+    await process(req, res, await repo.loadTextByPath(tree, path), req.ref ? cache_short : cache_long, tree, options) ? NEXT : ROUTE;
   },
 
   loadBlob: async (req, res) => {
     const { blob } = req.params;
+    const { cache_long = CACHE_LONG } = options;
 
-    const result = await repo.loadText(blob);
-    if (result) {
-      if (options.cache) {
-        res.set({
-          'Cache-Control': options.cache_long || CACHE_LONG,
-          'ETag': blob
-        });
-      }
-      if (options.mime) {
-        res.type(mime.lookup(req.path) || TYPE);
-      }
-      res.send(result);
-    }
-
-    return result ? NEXT : ROUTE;
+    await process(req, res, await repo.loadText(blob), cache_long, tree, options) ? NEXT : ROUTE;
   }
 });
