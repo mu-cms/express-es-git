89ec1de7f46a302a3342a37a85ce66b4c0eb8c6c
Mikael Karon <mikael@karon.se>
2018-09-29 01:38:52 +0200

    spread args onto controller

--- router.js
+++ router.js
@@ -1,9 +1,9 @@
 const PromiseRouter = require('express-promise-router');
 const param = require('./param');
 
-module.exports = (repo, options) => {
+module.exports = (...args) => {
   const router = new PromiseRouter();
-  const controller = require('./controller')(repo, options);
+  const controller = require('./controller')(...args);
 
   router.param('hash', param.sha1);
   router.get('/:hash\.:ext?', controller.loadText);
