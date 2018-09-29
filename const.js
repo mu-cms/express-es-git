89420c413c30f42e347f46e1cc8140702a690480
Mikael Karon <mikael@karon.se>
2018-09-29 03:00:17 +0200

    better naming of cache_control

--- const.js
+++ const.js
@@ -1,5 +1,5 @@
 const mime = require('mime-types');
-const cache = {
+const cache_control = {
   'refs/tags': 'max-age=1000, s-maxage=3000, stale-while-revalidate=5000, stale-if-error=1000',
   'refs/heads': 'max-age=100, s-maxage=300, stale-while-revalidate=500, stale-if-error=1000'
 };
@@ -9,6 +9,6 @@ exports.ROUTE = 'route';
 exports.REFS = [ 'refs/tags', 'refs/heads' ];
 exports.MIME = (path, type = 'text/plain') => mime.lookup(path) || type;
 exports.CACHE = (etag, ref) => ({
-  'Cache-Control': cache[ref] || 'only-if-cache',
+  'Cache-Control': cache_control[ref] || 'only-if-cache',
   'ETag': etag
 });
